48f8306e0dd8138b6ca394572368ebc81e4a4b9a
BrigittaArvay <43469779+BrigittaArvay@users.noreply.github.com>
2019-06-25 21:44:31 +0200

    JWT Token verification and user data access added.

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -29,7 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/signin").permitAll() // allowed by anyone
                 .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated() // allowed only when signed in
                 .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN") // allowed if signed in with ADMIN role
-                .anyRequest().denyAll(); // anything else is denied
+                .antMatchers(HttpMethod.GET, "/me").hasRole("ADMIN")
+                .anyRequest().denyAll() // anything else is denied
+                // new part
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
