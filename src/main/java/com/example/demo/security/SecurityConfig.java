06bbab39855621273c55f1cd39ec1f23b9ea07d1
BrigittaArvay <43469779+BrigittaArvay@users.noreply.github.com>
2019-06-25 21:16:28 +0200

    Jwt based login added.

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,14 +25,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                    .authorizeRequests()
-                    // allowed by anyone
-                    .antMatchers("/auth/signin").permitAll()
-                    // allowed only when signed in
-                    .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated()
-                    // allowed only if signed in with ADMIN role
-                    .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN")
-                    // anything else is denied
-                    .anyRequest().denyAll();
+                .authorizeRequests()
+                .antMatchers("/auth/signin").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated() // allowed only when signed in
+                .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN") // allowed if signed in with ADMIN role
+                .anyRequest().denyAll(); // anything else is denied
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
